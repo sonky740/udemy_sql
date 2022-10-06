d6f6de6bd1ad5d11f436a8ad5ac9df8e4ea309b0
손기연 <sonky740@ex-em.com>
2022-10-06 16:08:12 +0900

    mysql practice

--- mysql/index.sql
+++ mysql/index.sql
@@ -1,1 +1,21 @@
- -- Active: 1664945899270@@sonky.myasustor.com@3306@cat_app
+ -- Active: 1664945899270@@sonky.myasustor.com@3306@udemy
+-- CREATE TABLE people (first_name CHAR(20), last_name CHAR(20), age SMALLINT);
+-- INSERT INTO
+--   people (first_name, last_name, age)
+-- VALUES
+--   ('Phillip', 'Frond', 38),
+--   ('Calvin', 'Fischoeder', 70);
+-- SHOW WARNINGS;
+-- CREATE TABLE cats (name VARCHAR(100) NOT NULL DEFAULT 'unnamed', age INT NOT NULL DEFAULT 1);
+-- INSERT INTO cats () VALUES ();
+-- ALTER TABLE cats ALTER COLUMN id SERIAL FIRST;
+-- CREATE TABLE
+--   employees (
+--     id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+--     first_name VARCHAR(255) NOT NULL,
+--     last_name VARCHAR(255) NOT NULL,
+--     middle_name VARCHAR(255),
+--     age INT NOT NULL,
+--     current_status VARCHAR(255) NOT NULL DEFAULT 'employed'
+--   );
+-- INSERT INTO employees (first_name, last_name, age) VALUES ('Dora', 'Smith', 58)
